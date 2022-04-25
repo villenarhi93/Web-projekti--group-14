e387ceb15dc9b5392c7547e78753a5486f8ec82b
Ville <80310586+villenarhi93@users.noreply.github.com>
2022-04-25 20:02:25 +0300

    added maantieto js

--- maantieto.js
+++ maantieto.js
@@ -1,4 +1,48 @@
 function check() {
-    const rightAnswers = "answer2", 
+    // Tehdään käyttäjän tulokselle muuttuja
 
+    var score = 0;
+
+    // Oikeat vastaukset
+
+    const rightAnswers = ["answer2", "answer8", "answer9", "answer15", "answer17"]
+
+    // Lähde: https://www.youtube.com/watch?v=rCVqQ8NKU2M
+
+    if (document.getElementById("answer2").checked) {
+        score++;
+    }
+
+    if (document.getElementById("answer8").checked) {
+        score++;
+    }
+
+    if (document.getElementById("answer9").checked) {
+        score++;
+    }
+    
+    if (document.getElementById("answer15").checked) {
+        score++;
+    }
+
+    if (document.getElementById("answer17").checked) {
+        score++;
+    }
+
+
+    if (score >= 5) {
+        document.getElementById("result").innerHTML = "HIENOA!"
+    }
+
+    if (score >= 4 ) {
+        document.getElementById("result").innerHTML = "HYVÄ!"
+    }
+
+    if (score >= 3) {
+        document.getElementById("result").innerHTML = "VIELÄ JÄI PARANNETTAVAA ENSI KERRALLE!"
+    }
+
+    if (score >= 1) {
+        document.getElementById("result").innerHTML = "PAREMPI ONNI ENSI KERRALLA!"
+    }
 }
