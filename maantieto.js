222ac19d71ba09830b4a12fca3591ddc31a332e2
Ville Närhi <villenarhi@hotmail.fi>
2022-04-30 17:36:07 +0300

    change background color

--- maantieto.js
+++ maantieto.js
@@ -5,11 +5,11 @@ function check() {
 
     // Oikeat vastaukset
 
-    document.getElementById("right1").style.backgroundColor = "green";
-    document.getElementById("right2").style.backgroundColor = "green";
-    document.getElementById("right3").style.backgroundColor = "green";
-    document.getElementById("right4").style.backgroundColor = "green";
-    document.getElementById("right5").style.backgroundColor = "green";
+    document.getElementById("right1").style.backgroundColor = "lightgreen";
+    document.getElementById("right2").style.backgroundColor = "lightgreen";
+    document.getElementById("right3").style.backgroundColor = "lightgreen";
+    document.getElementById("right4").style.backgroundColor = "lightgreen";
+    document.getElementById("right5").style.backgroundColor = "lightgreen";
 
     // Lähde: https://www.youtube.com/watch?v=rCVqQ8NKU2M
 
