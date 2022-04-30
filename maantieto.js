f4f0bb1a597bd9f4cf40a0a8620d2b4f868cb5a7
Ville Närhi <villenarhi@hotmail.fi>
2022-04-30 16:03:44 +0300

    added returns

--- maantieto.js
+++ maantieto.js
@@ -31,24 +31,29 @@ function check() {
 
     // Palaute käyttäjälle
     
-    if (score = 5) {
+    if (score === 5) {
         document.getElementById("result").innerHTML =  "Hienoa, sait kaikki viisi vastausta oikein!";
+        return;
     }
 
-    if (score = 4) {
+    if (score === 4) {
         document.getElementById("result").innerHTML =  "Hienoa, sait 4/5 oikein!";
+        return;
     }
 
-    if (score = 3) {
+    if (score === 3) {
         document.getElementById("result").innerHTML =  "Hyvä suoritus, sait 3/5 oikein.";
+        return;
     }
 
-    if (score = 2 ) {
+    if (score === 2 ) {
         document.getElementById("result").innerHTML = "Sait 2/5 oikein, vielä jäi parannettavaa ensi kerralle.";
+        return;
     }
 
-    if (score = 1 ) {
+    if (score === 1 ) {
         document.getElementById("result").innerHTML = "Sait 1/5 oikein, vielä jäi parannettavaa ensi kerralle.";
+        return;
     }
 
    else {
