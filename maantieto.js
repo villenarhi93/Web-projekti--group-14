e016a8ee23553d1247e7eae9e8f2866df757da43
Ville Närhi <villenarhi@hotmail.fi>
2022-05-04 23:00:46 +0300

    small changes

--- maantieto.js
+++ maantieto.js
@@ -12,8 +12,8 @@ function check() {
     document.getElementById("info5").textContent = "Vastaus on väärin";
 
 
-    if (document.getElementById("answer1").checked) {
-        document.getElementById("wrong1").style.backgroundColor = "lightred";
+    if (document.getElementsByClassName("wrong").checked) {
+        document.getElementsByClassName("wrong").style.backgroundColor = "lightred";
     } 
     // Lähde: https://www.youtube.com/watch?v=rCVqQ8NKU2M
     // Jos vastaus on oikein näytetään myös selostus sekä tausta väri vaihdetaan
